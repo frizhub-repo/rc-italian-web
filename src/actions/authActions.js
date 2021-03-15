9ed1f3ebca6e3bb5c1ea8a014ba296109d06e514
nauman306 <m.nauman@frizhub.com>
2021-03-15 20:18:08 +0500

    toast added when success

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,14 +1,25 @@
+import { toast } from "react-toastify";
 import axiosIntance from "../utils/axios-configured";
 
 export const customerSignUp = (payload) => (dispatch) => {
   axiosIntance.post("/api/v1/customers/auth/signUp", payload).then((res) => {
-    authConfig(res.data.data.token);
+    if (res.status === 200) {
+      toast.success("You have been sign up successfully");
+      axiosIntance.defaults.headers.common["Authorization"] =
+        res.data.data.token;
+      authConfig(res.data.data.token);
+    }
   });
 };
 
 export const customerSignIn = (payload) => (dispatch) => {
   axiosIntance.post("/api/v1/customers/auth/login", payload).then((res) => {
-    authConfig(res.data.data.token);
+    if (res.status === 200) {
+      toast.success("You have been sign in successfully");
+      axiosIntance.defaults.headers.common["Authorization"] =
+        res.data.data.token;
+      authConfig(res.data.data.token);
+    }
   });
 };
 
