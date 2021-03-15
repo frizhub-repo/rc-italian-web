3151435f8c5d0c6d050f310b7b4a28c792a98a59
nauman306 <m.nauman@frizhub.com>
2021-03-15 20:18:27 +0500

    toast added when success response

--- src/actions/reserveTableActions.js
+++ src/actions/reserveTableActions.js
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import axiosIntance from "../utils/axios-configured";
 import { RESERVE_TABLE } from "../utils/types";
 
@@ -5,6 +6,9 @@ export const reserveTable = (payload) => (dispatch) => {
   axiosIntance
     .post("/api/v1/reservations/create/customers", payload)
     .then((res) => {
-      dispatch({ type: RESERVE_TABLE, payload: res.data });
+      if (res.status === 200) {
+        toast.success("Reservation has been created successfully!");
+        dispatch({ type: RESERVE_TABLE, payload: res.data });
+      }
     });
 };
