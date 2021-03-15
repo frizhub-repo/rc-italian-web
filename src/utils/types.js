ad11c3bfeb7cb9616b93d53faa349daeafbf6fce
nauman306 <m.nauman@frizhub.com>
2021-03-15 14:46:28 +0500

    types added for reservation table

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,6 +1,9 @@
 // APP types
-export const GET_RES_INFO = "RES INFO"
+export const GET_RES_INFO = "RES INFO";
 
 // Loading types
-export const START_LOADING = "START LOADING"
-export const STOP_LOADING = "STOP LOADING"
+export const START_LOADING = "START LOADING";
+export const STOP_LOADING = "STOP LOADING";
+
+// Table reservation types
+export const RESERVE_TABLE = "RESERVE TABLE";
