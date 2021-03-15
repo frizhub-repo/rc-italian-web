aa8c32e317028ad2cb064c4ce7d5f1f8f5a59464
nauman306 <m.nauman@frizhub.com>
2021-03-15 14:45:39 +0500

    root reducer added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import orderReducer from "./orderReducer";
-import appReducer from './appReducer';
-import loadingReducer from './loadingReducer';
+import appReducer from "./appReducer";
+import loadingReducer from "./loadingReducer";
+import reservationReducer from "./reservationReducer";
 
 export default combineReducers({
-    orders: orderReducer,
-    appReducer: appReducer,
-    loadingReducer: loadingReducer
-})
+  orders: orderReducer,
+  appReducer: appReducer,
+  loadingReducer: loadingReducer,
+  reservationReducer: reservationReducer,
+});
