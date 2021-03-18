6055f4f9b3c050b2195e85466e46b4d5558d6da0
nauman306 <m.nauman@frizhub.com>
2021-03-18 21:09:01 +0500

    api base url added

--- src/Components/Delivery/Product.js
+++ src/Components/Delivery/Product.js
@@ -29,7 +29,7 @@ function Product({ product }) {
           <img
             alt="ecommerce"
             className="lg:w-1/3 w-full h-24 object-cover object-center rounded"
-            src={process.env.REACT_APP_LOCAL_URL + "/" + product.images[0]}
+            src={process.env.REACT_APP_API_BASE_URL + "/" + product.images[0]}
           />
           <div className="lg:w-1/2 w-full px-3   flex-grow-1">
             <div className="d-flex w-full justify-content-between ">
