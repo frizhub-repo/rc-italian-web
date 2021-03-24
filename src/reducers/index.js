b4b035dc851286429de7ddfeb8ffc924473d44ea
nauman306 <m.nauman@frizhub.com>
2021-03-24 20:49:55 +0500

    added productReducer in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import orderReducer from "./orderReducer";
 import appReducer from "./appReducer";
 import loadingReducer from "./loadingReducer";
 import reservationReducer from "./reservationReducer";
+import productReducer from "./productReducer";
 
 export default combineReducers({
   orders: orderReducer,
   appReducer: appReducer,
   loadingReducer: loadingReducer,
   reservationReducer: reservationReducer,
+  productReducer: productReducer
 });
