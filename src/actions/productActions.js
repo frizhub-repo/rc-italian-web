4cfab0ca60d5d88e7cbf94c22fc45bbedd14959e
nauman306 <m.nauman@frizhub.com>
2021-03-25 20:31:21 +0500

    API URL changes

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,7 +3,7 @@ import axiosIntance from "../utils/axios-configured";
 import { GET_PRODUCT_BY_CATEGORY } from "../utils/types";
 
 export const getProductByCategory = (payload) => async (dispatch) => {
-  const res = await axiosIntance.get("/api/v1/product/category");
+  const res = await axiosIntance.get("/api/v1/products/category/public");
   if (res?.status === 200) {
     dispatch({ type: GET_PRODUCT_BY_CATEGORY, payload: res.data });
   }
