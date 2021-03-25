17cfc3364393f1085d370d28dfd8240701b544c3
nauman306 <m.nauman@frizhub.com>
2021-03-25 20:30:27 +0500

    Get restaurant info

--- src/actions/appActions.js
+++ src/actions/appActions.js
@@ -1,10 +1,8 @@
 import axiosIntance from "../utils/axios-configured";
 import { GET_RES_INFO } from "../utils/types";
 
-export const fetchAllResInfo = (clientId) => (dispatch) => {
-  axiosIntance
-    .get(`/api/v1/app/public/restaurant/logo/${clientId}`)
-    .then((res) => {
-      dispatch({ type: GET_RES_INFO, payload: res.data });
-    });
+export const fetchAllResInfo = () => (dispatch) => {
+  axiosIntance.get("/api/v1/app/public/restaurant").then((res) => {
+    dispatch({ type: GET_RES_INFO, payload: res.data });
+  });
 };
