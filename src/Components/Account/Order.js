67e449dba3b9c4ceccdff5096c908c375dc3631a
nauman306 <m.nauman@frizhub.com>
2021-03-31 11:39:58 +0500

    customers order page

---
+++ src/Components/Account/Order.js
@@ -0,0 +1,72 @@
+import {
+  Divider,
+  Grid,
+  makeStyles,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    // maxWidth: 500,
+  },
+  image: {
+    width: 200,
+    height: 120,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+export default function Order() {
+  const classes = useStyles();
+  return (
+    <Grid>
+      <h4>Orders</h4>
+      <Divider />
+      <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
+        <Grid item style={{ width: "100%" }}>
+          <Paper className={classes.paper}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <div className={classes.image}>
+                  <img
+                    className={classes.img}
+                    alt="complex"
+                    src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
+                  />
+                </div>
+              </Grid>
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs>
+                    <Typography gutterBottom variant="subtitle1">
+                      Standard license
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      Full resolution 1920x1080 • JPEG
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                      ID: 1030114
+                    </Typography>
+                    <Typography variant="body2" style={{ cursor: "pointer" }}>
+                      Remove
+                    </Typography>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Typography variant="subtitle1">$19.00</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
