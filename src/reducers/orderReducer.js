0943e6b35c743bab5ca81dc3bbd620baa529a505
nauman306 <m.nauman@frizhub.com>
2021-03-31 19:12:54 +0500

    add quantity to existence product if already in order

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -8,10 +8,25 @@ const initialState = {
 export default function (state = initialState, action) {
   switch (action.type) {
     case "ADD_ITEM":
-      return {
-        ...state,
-        items: state.items.concat(action.payload),
-      };
+      const index1 = state.items
+        .map(function (e) {
+          return e.product;
+        })
+        .indexOf(action.payload.product);
+      if (index1 !== -1) {
+        const items = state.items;
+        items[index1].quantity =
+          parseInt(items[index1].quantity) + parseInt(action.payload.quantity);
+        return { ...state, items };
+      } else
+        return {
+          ...state,
+          items: state.items.concat(action.payload),
+        };
+    // return {
+    //   ...state,
+    //   items: state.items.concat(action.payload),
+    // };
     case "REMOVE_ITEM":
       const index = state.items.indexOf(action.payload);
       return {
