8871fe29d416a30cd86670e24f1ee720cc71b717
nauman306 <m.nauman@frizhub.com>
2021-03-31 14:29:45 +0500

    Show orders items

--- src/Components/Account/Order.js
+++ src/Components/Account/Order.js
@@ -1,11 +1,14 @@
 import {
+  Button,
   Divider,
   Grid,
   makeStyles,
   Paper,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import axiosIntance from "../../utils/axios-configured";
 const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
@@ -25,49 +28,70 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function Order() {
   const classes = useStyles();
+  const { loading } = useSelector((state) => state.loadingReducer);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    axiosIntance
+      .get("/api/v1/orders/customers")
+      .then((res) => setOrders(res?.data?.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Grid>
       <h4>Orders</h4>
       <Divider />
       <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
-        {[1, 2, 3, 4].map((item) => (
-          <Grid item style={{ width: "100%" }}>
-            <Paper className={classes.paper}>
-              <Grid container spacing={2}>
-                <Grid item>
-                  <div className={classes.image}>
-                    <img
-                      className={classes.img}
-                      alt="complex"
-                      src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
-                    />
-                  </div>
-                </Grid>
-                <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                      <Typography gutterBottom variant="subtitle1">
-                        Standard license
-                      </Typography>
-                      <Typography variant="body2" gutterBottom>
-                        Full resolution 1920x1080 • JPEG
-                      </Typography>
-                      <Typography variant="body2" color="textSecondary">
-                        ID: 1030114
-                      </Typography>
-                      <Typography variant="body2" style={{ cursor: "pointer" }}>
-                        Remove
+        {loading && "Loading"}
+        {orders.length > 0 &&
+          orders.map((order, index) => (
+            <Grid item style={{ width: "100%" }} key={index}>
+              <Paper className={classes.paper}>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <div className={classes.image}>
+                      <img
+                        className={classes.img}
+                        alt="complex"
+                        src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
+                      />
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+                      <Grid item xs>
+                        <Typography gutterBottom variant="subtitle1">
+                          {order?.products?.map((odr) => (
+                            <span>
+                              {odr?.quantity}x {odr?.product?.title}{" "}
+                            </span>
+                          ))}
+                        </Typography>
+                        <Typography variant="body2" gutterBottom>
+                          Full resolution 1920x1080 • JPEG
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                          ID: 1030114
+                        </Typography>
+                        <Button
+                          size="small"
+                          style={{ backgroundColor: "#E2E2E2" }}
+                        >
+                          Reorder
+                        </Button>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="subtitle1">
+                        ${order?.total}
                       </Typography>
                     </Grid>
                   </Grid>
-                  <Grid item>
-                    <Typography variant="subtitle1">$19.00</Typography>
-                  </Grid>
                 </Grid>
-              </Grid>
-            </Paper>
-          </Grid>
-        ))}
+              </Paper>
+            </Grid>
+          ))}
       </Grid>
     </Grid>
   );
