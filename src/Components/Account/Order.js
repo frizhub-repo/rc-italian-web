5e5e45a108c48776736f807731f8d3aadb41f0b1
nauman306 <m.nauman@frizhub.com>
2021-03-31 13:06:45 +0500

    Show multiple order in list

--- src/Components/Account/Order.js
+++ src/Components/Account/Order.js
@@ -30,42 +30,44 @@ export default function Order() {
       <h4>Orders</h4>
       <Divider />
       <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
-        <Grid item style={{ width: "100%" }}>
-          <Paper className={classes.paper}>
-            <Grid container spacing={2}>
-              <Grid item>
-                <div className={classes.image}>
-                  <img
-                    className={classes.img}
-                    alt="complex"
-                    src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
-                  />
-                </div>
-              </Grid>
-              <Grid item xs={12} sm container>
-                <Grid item xs container direction="column" spacing={2}>
-                  <Grid item xs>
-                    <Typography gutterBottom variant="subtitle1">
-                      Standard license
-                    </Typography>
-                    <Typography variant="body2" gutterBottom>
-                      Full resolution 1920x1080 • JPEG
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary">
-                      ID: 1030114
-                    </Typography>
-                    <Typography variant="body2" style={{ cursor: "pointer" }}>
-                      Remove
-                    </Typography>
+        {[1, 2, 3, 4].map((item) => (
+          <Grid item style={{ width: "100%" }}>
+            <Paper className={classes.paper}>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <div className={classes.image}>
+                    <img
+                      className={classes.img}
+                      alt="complex"
+                      src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
+                    />
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm container>
+                  <Grid item xs container direction="column" spacing={2}>
+                    <Grid item xs>
+                      <Typography gutterBottom variant="subtitle1">
+                        Standard license
+                      </Typography>
+                      <Typography variant="body2" gutterBottom>
+                        Full resolution 1920x1080 • JPEG
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary">
+                        ID: 1030114
+                      </Typography>
+                      <Typography variant="body2" style={{ cursor: "pointer" }}>
+                        Remove
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="subtitle1">$19.00</Typography>
                   </Grid>
                 </Grid>
-                <Grid item>
-                  <Typography variant="subtitle1">$19.00</Typography>
-                </Grid>
               </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
+            </Paper>
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
