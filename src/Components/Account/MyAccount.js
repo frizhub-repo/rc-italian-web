b937b874e0dee56b05c69dc91906596895efa5f9
nauman306 <m.nauman@frizhub.com>
2021-03-31 13:00:27 +0500

    My Account form added

--- src/Components/Account/MyAccount.js
+++ src/Components/Account/MyAccount.js
@@ -1,17 +1,40 @@
-import { Divider, Grid, TextField } from "@material-ui/core";
+import {
+  Divider,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: "10px",
+  },
+  changePassword: {
+    marginTop: "15px",
+    color: "#B39872",
+  },
+  bgColor: {
+    backgroundColor: "white",
+  },
+}));
+
 export default function MyAccount() {
+  const classes = useStyles();
+
   return (
     <Grid>
       <h4>My Account</h4>
       <Divider />
-      <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
+      <Grid container direction="row" spacing={2} className={classes.root}>
         <Grid item md={6}>
           <TextField
             size="small"
             fullWidth
+            className={classes.bgColor}
             id="outlined-basic"
-            placeholder="hellow world"
+            placeholder="Name"
             variant="outlined"
           />
         </Grid>
@@ -19,20 +42,23 @@ export default function MyAccount() {
           <TextField
             size="small"
             fullWidth
+            className={classes.bgColor}
             id="outlined-basic"
-            placeholder="hellow world"
+            placeholder="Username"
             variant="outlined"
           />
         </Grid>
       </Grid>
 
-      <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
+      <Grid container direction="row" spacing={2} className={classes.root}>
         <Grid item md={6}>
           <TextField
             size="small"
             fullWidth
+            className={classes.bgColor}
             id="outlined-basic"
-            placeholder="hellow world"
+            placeholder="Email"
+            type="email"
             variant="outlined"
           />
         </Grid>
@@ -40,10 +66,34 @@ export default function MyAccount() {
           <TextField
             size="small"
             fullWidth
+            className={classes.bgColor}
             id="outlined-basic"
-            placeholder="hellow world"
+            placeholder="Phone Number"
             variant="outlined"
           />
+        </Grid>
+      </Grid>
+      <Grid container direction="row" spacing={2} className={classes.root}>
+        <Grid item md={12}>
+          <TextField
+            size="small"
+            fullWidth
+            className={classes.bgColor}
+            id="outlined-basic"
+            placeholder="Password"
+            type="password"
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="flex-end"
+        className={classes.changePassword}
+      >
+        <Grid item>
+          <Typography>Change Password</Typography>
         </Grid>
       </Grid>
     </Grid>
