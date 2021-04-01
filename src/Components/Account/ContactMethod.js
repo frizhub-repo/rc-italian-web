3ec8f6fd2c70e42083aa20226c70af05b985770e
nauman306 <m.nauman@frizhub.com>
2021-04-01 15:59:10 +0500

    Customer contact method -  design

--- src/Components/Account/ContactMethod.js
+++ src/Components/Account/ContactMethod.js
@@ -1,12 +1,54 @@
-import { Divider, Grid } from "@material-ui/core";
+import {
+  Divider,
+  FormControlLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 export default function ContactMethod() {
+  const [value, setValue] = React.useState("email");
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <Grid>
       <h4>Contact Method</h4>
       <Divider />
-      <Grid container direction="row" spacing={2} style={{ marginTop: "10px" }}>
-        <h1>My Orders</h1>
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+        style={{ marginTop: "10px" }}
+      >
+        <Grid item>
+          <Typography
+            variant="subtitle1"
+            style={{ fontSize: "20px" }}
+            gutterBottom
+          >
+            I would like to receive updates via
+          </Typography>
+          <RadioGroup
+            aria-label="contactMethod"
+            name="contactMethod"
+            value={value}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="sms"
+              control={<Radio />}
+              label="SMS (Important update only)"
+            />
+            <FormControlLabel
+              value="email"
+              control={<Radio />}
+              label="Email (Order Detail are send by email)"
+            />
+          </RadioGroup>
+        </Grid>
       </Grid>
     </Grid>
   );
