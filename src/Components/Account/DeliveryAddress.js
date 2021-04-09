9c28c0d5dd56a2029d152993a2e1d4831330bd13
nauman306 <m.nauman@frizhub.com>
2021-04-09 16:40:08 +0500

    Add/show delivery address and minor css changes

--- src/Components/Account/DeliveryAddress.js
+++ src/Components/Account/DeliveryAddress.js
@@ -1,13 +1,16 @@
 import {
+  Button,
   Divider,
   Grid,
   makeStyles,
   Paper,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@material-ui/icons/DeleteForever";
 import EditIcon from "@material-ui/icons/Create";
+import { useUserContext } from "../../Context/userContext";
+import DeliveryAddressDialog from "../Common/DeliveryAddressModal";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -15,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
   },
   containr: {
-    marginTop: "10px"
+    marginTop: "10px",
   },
   root: {
     width: "100%",
@@ -32,68 +35,114 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "30px",
     padding: "3px",
     marginRight: "5px",
-    cursor: 'pointer'
+    cursor: "pointer",
   },
   deleteIcon: {
     backgroundColor: "#FADBDB",
     borderRadius: "20px",
     padding: "2px",
-    cursor: 'pointer'
+    cursor: "pointer",
   },
   phone: {
-    marginTop: "13px"
+    marginTop: "13px",
   },
   action: {
-    display: "flex"
+    display: "flex",
   },
-  text:{
-    marginRight: "30px"
-  }
+  text: {
+    marginRight: "30px",
+  },
+  addressKey: {
+    fontSize: "15px",
+    textTransform: 'capitalize'
+  },
+  addAddressButton: {
+    borderRadius: "30px",
+    padding: "0px 12px",
+    backgroundColor: "black",
+    color: "white",
+    margin: "2px",
+    "&:hover": {
+      backgroundColor: "black",
+      color: "white",
+    },
+  },
 }));
 
 export default function DeliveryAddress() {
   const classes = useStyles();
+  const { customer } = useUserContext();
+  const [openDelivery, setOpenDelivery] = useState(false);
 
   return (
     <Grid>
-      <h4>Delivery Address</h4>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h4>Delivery Address</h4>
+        <Button
+          disableElevation
+          className={classes.addAddressButton}
+          type="submit"
+          onClick={() => {setOpenDelivery(true)}}
+        >
+          Add New Address
+        </Button>
+      </div>
       <Divider />
       <Grid container direction="row" spacing={2} className={classes.containr}>
-        {[1, 2, 3, 4].map((item, index) => (
-          <Grid item className={classes.root} key={index}>
-            <Paper className={classes.paper}>
-              <div className={classes.main}>
-                <div className={classes.addressCard}>
-                  <div>
-                    <Typography variant="h6">Artisanal Kale</Typography>
-                    <Typography
-                      variant="body2"
-                      gutterBottom
-                      className={classes.text}
-                    >
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Quos blanditiis tenetur unde suscipit, quam beatae rerum
-                      inventore consectetur, neque doloribus, cupiditate numquam
-                      dignissimos laborum fugiat deleniti? Eum quasi quidem
-                      quibusdam.
-                    </Typography>
-                    <Typography className={classes.phone} variant="body2">
-                      Phone: <b>+001222333454</b>
-                    </Typography>
-                    <Typography variant="body2">
-                      Email: amond@gmail.com
-                    </Typography>
-                  </div>
-                  <div className={classes.action}>
-                    <EditIcon className={classes.editIcon} />
-                    <DeleteIcon className={classes.deleteIcon} />
+        {customer &&
+          customer?.addresses?.map((address, index) => (
+            <Grid item className={classes.root} key={index}>
+              <Paper className={classes.paper}>
+                <div className={classes.main}>
+                  <div className={classes.addressCard}>
+                    <div>
+                      <Typography variant="h6">
+                        {address?.name}{" "}
+                        <span className={classes.addressKey}>
+                          ({address?.addressKey})
+                        </span>
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        gutterBottom
+                        className={classes.text}
+                      >
+                        <b>Address Line 1: </b>
+                        {address?.addressLine1} {address?.city},{" "}
+                        {address?.zipOrPostalCode} {address?.stateOrProvince},{" "}
+                        {address?.country}
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        gutterBottom
+                        className={classes.text}
+                      >
+                        <b>Address Line 2: </b>
+                        {address?.addressLine2} {address?.city},{" "}
+                        {address?.zipOrPostalCode} {address?.stateOrProvince},{" "}
+                        {address?.country}
+                      </Typography>
+                      <Typography variant="body2" className={classes.phone}>
+                        <b>Phone:</b> {address?.phoneNumber}
+                      </Typography>
+                      <Typography variant="body2">
+                        <b>Email:</b> {customer?.email}
+                      </Typography>
+                    </div>
+                    <div className={classes.action}>
+                      <EditIcon className={classes.editIcon} />
+                      <DeleteIcon className={classes.deleteIcon} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Paper>
-          </Grid>
-        ))}
+              </Paper>
+            </Grid>
+          ))}
       </Grid>
+      <DeliveryAddressDialog
+        openDelivery={openDelivery}
+        setOpenDelivery={setOpenDelivery}
+      />
     </Grid>
   );
 }
