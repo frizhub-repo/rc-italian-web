e304fc4b16d2beeb689ba56b8d527a1be7445f7c
nauman306 <m.nauman@frizhub.com>
2021-04-09 20:15:56 +0500

    Place holder added

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -73,7 +73,7 @@ function Contact() {
                                className="w-full bg-white  border border-gray-300 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                     </div>
                     <div className="relative mb-4" placeholder='Message'>
-                        <textarea id="message" name="message"
+                        <textarea id="message" name="message" placeholder="Message"
                                   className="w-full bg-white  border border-gray-300 focus:border-indigo-500 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                     </div>
                     <button
