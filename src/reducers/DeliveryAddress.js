237b3ba4272aa4e9cf55510da115f29acf405afe
nauman306 <m.nauman@frizhub.com>
2021-04-10 12:57:13 +0500

    Delete delivery address from store list

--- src/reducers/DeliveryAddress.js
+++ src/reducers/DeliveryAddress.js
@@ -1,4 +1,4 @@
-import { GET_DELIVERY_ADDRESS } from "../utils/types";
+import { GET_DELIVERY_ADDRESS, DELETE_DELIVERY_ADDRESS } from "../utils/types";
 const initialState = {
   deliveryAddress: [],
 };
@@ -7,6 +7,13 @@ export default function (state = initialState, actions) {
   switch (actions.type) {
     case GET_DELIVERY_ADDRESS:
       return { ...state, deliveryAddress: actions.payload };
+    case DELETE_DELIVERY_ADDRESS:
+      return {
+        ...state,
+        deliveryAddress: state.deliveryAddress.filter(
+          (item) => item._id !== actions.payload
+        ),
+      };
     default:
       return state;
   }
