f7cf375688509181250fcf6296c87c0744dc8b55
se-Haroon <haroon.rasheed@frizhub.com>
2021-08-16 16:14:34 +0500

    reservation: resolving testimonial import issue

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -6,7 +6,7 @@ import Story from "./Story";
 import FindUs from "./FindUs";
 import OpeningHours from "./OpeningHours";
 import Gallery from "../Body/Gallery";
-import Testimonial from "../Body/Testimonial";
+import Testimonial from "../Body/testimonial";
 
 function Main() {
   return (
