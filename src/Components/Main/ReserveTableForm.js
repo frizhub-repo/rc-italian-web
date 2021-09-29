82b5dc51dd4e497cda0c36eda412881312d6a1aa
naumanmallik <m.nauman@frizhub.com>
2021-09-29 16:28:25 +0500

    Create reservation in Home page

--- src/Components/Main/ReserveTableForm.js
+++ src/Components/Main/ReserveTableForm.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { reserveTable } from "api/customer";
+import { toast } from "react-toastify";
+import { CircularProgress } from "@material-ui/core";
 
 const useStyle = () => ({
   container: {
@@ -16,6 +22,7 @@ const useStyle = () => ({
     borderRadius: "23px",
   },
   label: {
+    flex: 1,
     marginTop: "20px",
     marginBotton: "15px",
     fontSize: "24px",
@@ -24,56 +31,70 @@ const useStyle = () => ({
     border: "1px solid #B29051",
     color: "#B29051",
   },
+  keyAlignMent: {
+    display: "flex",
+    textAlign: "left",
+    marginLeft: "10px",
+  },
+  progressbarSpacing: {
+    marginRight: "8px",
+  },
+  datePickerRoot: {
+    display: "flex",
+    alignItems: "center",
+  },
 });
+
+const today = new Date();
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+const nextTomorrow = new Date(today);
+nextTomorrow.setDate(nextTomorrow.getDate() + 2);
+
+const timeSlots = {
+  Breakfast: ["11:30", "12:00", "12:30", "13:00", "13:30"],
+  Dinner: ["19:30", "20:00", "20:30", "21:00", "21:30"],
+};
 
 export default function ReserveTableForm() {
   const styles = useStyle();
-  const [people, setPeople] = useState([
-    { num: "1", selected: false },
-    { num: "2", selected: false },
-    { num: "3", selected: true },
-    { num: "4+", selected: false },
-  ]);
-  const [meals, setMeals] = useState([
-    { meal: "Breakfast", selected: false },
-    { meal: "Lunch", selected: true },
-    { meal: "Dinner", selected: false },
-  ]);
-  const [times, setTimes] = useState([
-    { time: "Today", selected: true },
-    { time: "Tomorrow", selected: false },
-    { time: "Choose", selected: false },
-  ]);
+  const [loading, setLoading] = useState(false);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [startDate, setStartDate] = useState(new Date());
+  const [reservation, setReservation] = useState({
+    numberOfPeople: 3,
+    services: "lunch",
+    timeSlot: "12:00",
+  });
 
-  function handleClickPeople(e) {
-    e.preventDefault();
-    setPeople(
-      people.map((p, index) => ({
-        num: p.num,
-        selected: e.target.id == index,
-      }))
-    );
+  const reserveTableHandler = async () => {
+    setLoading(true);
+    try {
+      const payload = { ...reservation, startTime: startDate };
+      console.log("payload :>> ", payload);
+      await reserveTable(payload);
+      toast.success("Reservation has been created successfully");
+      setLoading(false);
+    } catch (error) {
+      toast.error("Error occured while creating reservation");
+      console.log({ error });
+      setLoading(false);
+    }
+  };
+
+  const handleChangeTimeSlot = (timeSlot) => {
+    setReservation((prev) => ({ ...prev, timeSlot }));
+  };
+
+  function handleClickPeople(people) {
+    setReservation((prev) => ({ ...prev, numberOfPeople: people }));
   }
 
-  function handleClickTime(e) {
-    e.preventDefault();
-    setTimes(
-      times.map((t, index) => ({
-        time: t.time,
-        selected: e.target.id == index,
-      }))
-    );
+  function handleClickMeal(m) {
+    setReservation((prev) => ({ ...prev, services: m }));
   }
 
-  function handleClickMeal(e) {
-    e.preventDefault();
-    setMeals(
-      meals.map((m, index) => ({
-        meal: m.meal,
-        selected: e.target.id == index,
-      }))
-    );
-  }
+  const handleShowCalendar = () => setShowCalendar(true);
 
   return (
     <div style={styles.container}>
@@ -95,20 +116,25 @@ export default function ReserveTableForm() {
         </div>
         <label style={styles.label}>Number of People</label>
         <div className="row">
-          {people.map((p, index) => {
+          {[1, 2, 3, 4].map((people, index) => {
             return (
               <div className="col-6 col-md-3">
                 <button
                   id={index}
                   className="btn btn-lg btn-outline"
-                  onClick={handleClickPeople}
+                  type="button"
+                  onClick={() => handleClickPeople(people)}
                   style={{
                     ...styles.button,
-                    background: p.selected ? "#B29051" : "white",
-                    color: p.selected ? "white" : "grey",
+                    background:
+                      reservation?.numberOfPeople === people
+                        ? "#B29051"
+                        : "white",
+                    color:
+                      reservation?.numberOfPeople === people ? "white" : "grey",
                   }}
                 >
-                  {p.num}
+                  {people}
                 </button>
               </div>
             );
@@ -116,56 +142,155 @@ export default function ReserveTableForm() {
         </div>
         <label style={styles.label}>Which one you want to reserve?</label>
         <div className="row">
-          {meals.map((m, index) => {
+          {["breakfast", "lunch", "dinner"].map((service, index) => {
             return (
               <div className="col-sm-6 col-md-4 col-lg-6 col-xl-4">
                 <button
                   id={index}
                   className="btn btn-lg btn-outline"
-                  onClick={handleClickMeal}
+                  type="button"
+                  onClick={() => handleClickMeal(service)}
                   style={{
                     ...styles.button,
-                    background: m.selected ? "#B29051" : "white",
-                    color: m.selected ? "white" : "grey",
+                    background:
+                      service === reservation?.services ? "#B29051" : "white",
+                    color: service === reservation?.services ? "white" : "grey",
                   }}
                 >
-                  {m.meal}
+                  {service}
                 </button>
               </div>
             );
           })}
         </div>
-        <label style={styles.label}>When you want to reserve?</label>
+        <div style={styles.datePickerRoot}>
+          <label style={styles.label}>When you want to reserve?</label>
+          {showCalendar ? (
+            <DatePicker
+              minDate={today}
+              selected={startDate}
+              onChange={(date) => {
+                setStartDate(date);
+              }}
+            />
+          ) : (
+            <div title="Choose date">
+              <CalendarTodayIcon onClick={handleShowCalendar} />
+            </div>
+          )}
+        </div>
         <div className="row">
-          {times.map((t, index) => {
-            return (
-              <div className="col-sm-6 col-md-4 col-lg-6 col-xl-4">
-                <button
-                  id={index}
-                  className="btn btn-lg btn-outline"
-                  onClick={handleClickTime}
-                  style={{
-                    ...styles.button,
-                    background: t.selected ? "#B29051" : "white",
-                    color: t.selected ? "white" : "grey",
-                  }}
-                >
-                  {t.time}
-                </button>
+          <div className="col-sm-6 col-md-4 col-lg-6 col-xl-4">
+            <button
+              className="btn btn-lg btn-outline"
+              type="button"
+              onClick={() => setStartDate(today)}
+              style={{
+                ...styles.button,
+                background:
+                  today.toLocaleDateString() === startDate.toLocaleDateString()
+                    ? "#B29051"
+                    : "white",
+                color:
+                  today.toLocaleDateString() === startDate.toLocaleDateString()
+                    ? "white"
+                    : "grey",
+              }}
+            >
+              Today
+            </button>
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-6 col-xl-4">
+            <button
+              className="btn btn-lg btn-outline"
+              type="button"
+              onClick={() => setStartDate(tomorrow)}
+              style={{
+                ...styles.button,
+                background:
+                  tomorrow.toLocaleDateString() ===
+                  startDate.toLocaleDateString()
+                    ? "#B29051"
+                    : "white",
+                color:
+                  tomorrow.toLocaleDateString() ===
+                  startDate.toLocaleDateString()
+                    ? "white"
+                    : "grey",
+              }}
+            >
+              Tomorrow
+            </button>
+          </div>
+          <div className="col-sm-6 col-md-4 col-lg-6 col-xl-4">
+            <button
+              className="btn btn-lg btn-outline"
+              type="button"
+              onClick={() => setStartDate(nextTomorrow)}
+              style={{
+                ...styles.button,
+                background:
+                  nextTomorrow.toLocaleDateString() ===
+                  startDate.toLocaleDateString()
+                    ? "#B29051"
+                    : "white",
+                color:
+                  nextTomorrow.toLocaleDateString() ===
+                  startDate.toLocaleDateString()
+                    ? "white"
+                    : "grey",
+              }}
+            >
+              {nextTomorrow.toLocaleDateString()}
+            </button>
+          </div>
+        </div>
+        <div>
+          <label style={styles.label}>Choose time slots</label>
+
+          {Object.entries(timeSlots).map(([key, value], index) => (
+            <div>
+              <label style={styles.keyAlignMent}>{key}</label>
+              <div>
+                {value?.map((time) => (
+                  <button
+                    type="button"
+                    className="btn btn-lg btn-outline"
+                    onClick={() => handleChangeTimeSlot(time)}
+                    style={{
+                      ...styles.button,
+                      background:
+                        reservation.timeSlot === time ? "#B29051" : "white",
+                      color: reservation.timeSlot === time ? "white" : "grey",
+                    }}
+                  >
+                    {time}
+                  </button>
+                ))}
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
         <button
+          type="button"
           className="btn btn-outline btn-lg btn-block mt-5"
           style={styles.button}
         >
           Schedule a Hour
         </button>
         <button
+          type="button"
+          onClick={reserveTableHandler}
           className="btn btn-outline btn-lg btn-block mt-3"
           style={{ ...styles.button, ...styles.secondaryBtn }}
         >
+          {loading && (
+            <CircularProgress
+              color="inherit"
+              size={20}
+              style={styles.progressbarSpacing}
+            />
+          )}
           Book your Reservation
         </button>
         <button></button>
